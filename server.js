ebe904a1dedd577e3bac0f782066e7e6678e7928
Simon Emanuel Schmid <simon@schmid.io>
2018-03-06 09:57:37 +0100

    Dgram copy/paste

---
+++ server.js
@@ -0,0 +1,21 @@
+const dgram = require('dgram');
+
+const discoveryServer = dgram.createSocket('udp4');
+
+discoveryServer.on('error', err => {
+  console.log(`discoveryServer error:\n${err.stack}`);
+  discoveryServer.close();
+});
+
+discoveryServer.on('message', (msg, rinfo) => {
+  console.log(
+    `discoveryServer got: ${msg} from ${rinfo.address}:${rinfo.port}`,
+  );
+});
+
+discoveryServer.on('listening', () => {
+  const address = discoveryServer.address();
+  console.log(`discoveryServer listening ${address.address}:${address.port}`);
+});
+
+discoveryServer.bind(1025);
