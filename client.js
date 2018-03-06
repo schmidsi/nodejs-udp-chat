e5f73c6ddb9f694da970efc58b2554e3c7fdab26
Simon Emanuel Schmid <simon@schmid.io>
2018-03-06 10:49:38 +0100

    Basic broadcast client implementation

--- client.js
+++ client.js
@@ -2,6 +2,20 @@ import dgram from 'dgram';
 
 const client = dgram.createSocket('udp4');
 
-client.send('Hallo welt', 1025, 'localhost', err => {
-  client.close();
+client.on('listening', function() {
+  var address = client.address();
+  console.log(
+    'UDP Client listening on ' + address.address + ':' + address.port,
+  );
+  // client.send('Hallo welt', 1025, 'localhost', err => {
+  //   // client.close();
+  // });
+  client.setBroadcast(true);
+  client.send('Broadcast', 1025, '255.255.255.255', err => {
+    if (err) console.error(err);
+    client.close();
+  });
 });
+
+// client.bind('10.207.5.95');
+client.bind();
